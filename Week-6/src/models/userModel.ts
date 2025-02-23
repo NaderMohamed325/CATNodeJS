d5c4ef5f709cb2d7a04aae79e1c0cd18100cd765
NaderMohamed325 <nadoorclasher@gmail.com>
2025-02-23 18:28:26 +0200

    Add user model with validation and password hashing

---
+++ Week-6/src/models/userModel.ts
@@ -0,0 +1,43 @@
+import mongoose, { CallbackError } from "mongoose";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" })
+    .max(50, { message: "Name cannot exceed 50 characters" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" })
+    .max(100, { message: "Password is too long" }),
+});
+
+const userMongooseSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userMongooseSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  }
+});
+
+const User = mongoose.model("User", userMongooseSchema);
+export { User,userSchema };
