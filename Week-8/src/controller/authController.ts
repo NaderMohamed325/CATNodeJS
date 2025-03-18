80061fae2cc4d4517c798b842d2345ccebf2119d
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-18 11:52:20 +0200

    Add loginUser function for user authentication with JWT

--- Week-8/src/controller/authController.ts
+++ Week-8/src/controller/authController.ts
@@ -2,7 +2,7 @@ import { User, userZodSchema } from '../models/userModel';
 import { catchAsync } from '../utils/catchAsync';
 import { Request, Response, NextFunction } from 'express';
 import { zodSchemaValidate } from '../utils/validation';
-
+import jwt from 'jsonwebtoken';
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   /*
   username: string;
@@ -37,4 +37,32 @@ const registerUser = catchAsync(async (req: Request, res: Response, next: NextFu
   });
 });
 
-export { registerUser };
+const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please fill the login form',
+    });
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECURE || 'My super secure key',
+      { expiresIn: '1h' }
+    );
+
+    return res.status(200).json({
+      token: token,
+      message: 'Login successful',
+    });
+  } else {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please create account before login',
+    });
+  }
+});
+export { registerUser, loginUser };
