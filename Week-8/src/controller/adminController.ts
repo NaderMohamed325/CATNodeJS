084fb836d8bb8ff5b0dc917abd34d1ebbba6e02e
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-22 23:41:55 +0200

    Implement admin login functionality with JWT authentication and cookie handling

--- Week-8/src/controller/adminController.ts
+++ Week-8/src/controller/adminController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { User } from '../models/userModel';
 import { AppError } from '../utils/appError';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const getAllUsers = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
   const Users = await User.find();
@@ -22,10 +24,48 @@ const getAllUsers = catchAsync(async (_req: Request, res: Response, _next: NextF
 });
 
 const adminLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const existingToken = req.cookies.token || req.headers.authorization?.split(' ')[1];
+  console.log(req.cookies.token); //needed cookies parser
+  if (existingToken) {
+    try {
+      jwt.verify(existingToken, process.env.JWT_SECURE as string);
+      return next(new AppError('You are already logged in', 400));
+    } catch (error) {
+      return next(new AppError('Invalid or expired token', 401));
+    }
+  }
+
   const { email, password } = req.body;
   if (!email || !password) {
-    next(new AppError('Please fill the admin form', 401));
+    return next(new AppError('Please fill the admin form', 401));
   }
+
+  const admin = await User.findOne({ email });
+  if (!admin) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, admin.password);
+  if (!isPasswordCorrect) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  if (admin.role === 'user') {
+    return next(new AppError('Incorrect email or password', 403));
+  }
+
+  const token = jwt.sign({ id: admin._id }, process.env.JWT_SECURE as string, { expiresIn: '1h' });
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 });
 
-export { getAllUsers };
+export { getAllUsers, adminLogin };
