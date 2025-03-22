385c188360bd9d51f677c9a2bbbb2aa272bf3300
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-22 23:10:48 +0200

    Add role field to user model with default value and validation

--- Week-8/src/models/userModel.ts
+++ Week-8/src/models/userModel.ts
@@ -2,9 +2,15 @@ import mongoose from 'mongoose';
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
 
+enum role {
+  'user',
+  admin,
+}
+
 interface IUser extends mongoose.Document {
   username: string;
   email: string;
+  role: string;
   password: string;
   createdAt: Date;
   updatedAt: Date;
@@ -15,6 +21,9 @@ const userSchema = new mongoose.Schema<IUser>(
     username: { type: String, required: true, minlength: 3 },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
+    role: { type: String, 
+      enum: Object.values(role), 
+      default: 'user' }, 
   },
   { timestamps: true }
 );
@@ -33,17 +42,19 @@ userSchema.pre(['findOneAndUpdate', 'updateOne', 'updateMany'], async function (
   if (update.password) {
     const salt = await bcrypt.genSalt(12);
     update.password = await bcrypt.hash(update.password, salt);
-    this.setUpdate(update); // Set the updated password
+    this.setUpdate(update);
   }
 
   next();
 });
+
 const User = mongoose.model<IUser>('User', userSchema);
 
 const userZodSchema = z.object({
   username: z.string().min(3, 'Username must be at least 3 characters'),
   email: z.string().email('Invalid email format'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
+  role: z.enum(['user', 'admin']).default('user'), 
 });
 
 export { User, userZodSchema };
