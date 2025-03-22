d0ffea822aa66af6274783167ee65a9284cd2cda
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-22 23:10:45 +0200

    Add admin login function with error handling for missing credentials

--- Week-8/src/controller/adminController.ts
+++ Week-8/src/controller/adminController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { User } from '../models/userModel';
+import { AppError } from '../utils/appError';
 
 const getAllUsers = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
   const Users = await User.find();
@@ -17,6 +18,13 @@ const getAllUsers = catchAsync(async (_req: Request, res: Response, _next: NextF
       status: 'fail',
       message: 'Error fetching data',
     });
+  }
+});
+
+const adminLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(new AppError('Please fill the admin form', 401));
   }
 });
 
