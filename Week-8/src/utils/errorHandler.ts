707253102128805306f8640ef7e1f9d1fd831113
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-23 11:39:37 +0200

    Clear authentication token cookie on error response

--- Week-8/src/utils/errorHandler.ts
+++ Week-8/src/utils/errorHandler.ts
@@ -3,7 +3,12 @@ import { AppError } from './appError';
 const errorHandler = (err: AppError, req: any, res: any, next: any) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
+  res.cookie('token', '', {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+    expires: new Date(0),
+  });
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
