7c3bdc3da7c6f6652f3e2a3a84b3354c811ff03d
NaderMohamed325 <nadoorclasher@gmail.com>
2025-04-12 18:05:41 +0200

    Refactor productController to improve getAllProducts function and add getOneProduct function with error handling

--- Week-8/src/controller/productController.ts
+++ Week-8/src/controller/productController.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { Product } from '../models/productModel';
+import { AppError } from '../utils/appError';
+import mongoose from 'mongoose';
 
 //- **GET /products** – Get all products
 
-const getAllProducts = catchAsync(async (req, res, next) => {
+const getAllProducts = catchAsync(async (_req, res, next) => {
   const products = await Product.find();
-  res.status(200).json({
+  return res.status(200).json({
     status: 'success',
     length: products.length,
     data: {
@@ -15,4 +17,24 @@ const getAllProducts = catchAsync(async (req, res, next) => {
   });
 });
 
-export { getAllProducts };
+//- **GET /products/{id}** – Get a single product
+const getOneProduct = catchAsync(async (req: Request, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError('Invalid product ID', 400));
+  }
+
+  const product = await Product.findById(id);
+  if (!product) {
+    return next(new AppError('No product with this id', 404));
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      product,
+    },
+  });
+});
+export { getAllProducts, getOneProduct };
