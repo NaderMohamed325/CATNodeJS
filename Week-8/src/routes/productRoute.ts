3b5608fdcb19a76e76c95c5dbee43af02647ccbf
NaderMohamed325 <nadoorclasher@gmail.com>
2025-04-12 17:36:28 +0200

    Add GET route for retrieving all products with authentication

--- Week-8/src/routes/productRoute.ts
+++ Week-8/src/routes/productRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { adminAuth } from '../utils/middlewares/adminAuth';
 import { adminCreateProduct } from '../controller/adminController';
+import { authenticate } from '../utils/validation';
+import { getAllProducts } from '../controller/productController';
 
 const productRouter = Router();
 
 productRouter.post('/products', adminAuth, adminCreateProduct);
+productRouter.get('/products', authenticate, getAllProducts);
 
-
-export {productRouter}
+export { productRouter };
