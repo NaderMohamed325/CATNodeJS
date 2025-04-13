5187f2006716329a683efef9c71ae10839853231
NaderMohamed325 <nadoorclasher@gmail.com>
2025-04-13 18:13:51 +0200

    Remove unused authentication middleware from validation utility

--- Week-8/src/utils/validation.ts
+++ Week-8/src/utils/validation.ts
@@ -11,22 +11,5 @@ const zodSchemaValidate = (data: any, schema: z.ZodSchema, res: Response) => {
   return result.data;
 };
 
-const authenticate = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.token;
 
-  if (!token) {
-    res.status(401).json({
-      message: 'Unauthorized , missing token',
-    });
-  }
-  const tokenStatus = jwt.verify(token as string, process.env.JWT_SECURE as string);
-  console.log(tokenStatus);
-  if (!tokenStatus) {
-    return res.status(401).json({
-      message: 'Unauthorized, invalid token',
-    });
-  }
-  next();
-});
-
-export { zodSchemaValidate, authenticate };
+export { zodSchemaValidate };
