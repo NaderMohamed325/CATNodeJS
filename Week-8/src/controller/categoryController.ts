99c5856858dbdb7bd6beca675d5bf7d4ece9ae8a
NaderMohamed325 <nadoorclasher@gmail.com>
2025-04-15 14:00:13 +0200

    Add admin category controller with create, get, and delete functionality

---
+++ Week-8/src/controller/categoryController.ts
@@ -0,0 +1,45 @@
+// controllers/adminCategoryController.ts
+import { Request, Response, NextFunction } from 'express';
+import { Category } from '../models/categoryModel';
+import { catchAsync } from '../utils/catchAsync';
+import { AppError } from '../utils/appError';
+
+// Create Category
+const adminCreateCategory = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+  if (!name) return next(new AppError('Category name is required', 400));
+
+  const exists = await Category.findOne({ name });
+  if (exists) return next(new AppError('Category already exists', 400));
+
+  const category = await Category.create({ name });
+
+  res.status(201).json({
+    status: 'success',
+    data: { category },
+  });
+});
+
+// Get All Categories
+const getAllCategories = catchAsync(async (_req, res) => {
+  const categories = await Category.find();
+  res.status(200).json({
+    status: 'success',
+    length: categories.length,
+    data: { categories },
+  });
+});
+
+// Delete Category
+const adminDeleteCategory = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const category = await Category.findByIdAndDelete(id);
+  if (!category) return next(new AppError('Category not found', 404));
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Category deleted',
+  });
+});
+
+export { adminCreateCategory, getAllCategories, adminDeleteCategory };
