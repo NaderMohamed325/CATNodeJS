eaaf98eca6db76786d86279724198620727437a8
NaderMohamed325 <nadoorclasher@gmail.com>
2025-04-16 11:19:06 +0200

    Refactor product controller for improved readability and consistency

--- Week-8/src/controller/productController.ts
+++ Week-8/src/controller/productController.ts
@@ -1,15 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
-import { catchAsync } from '../utils/catchAsync';
-import { Product } from '../models/productModel';
-import { AppError } from '../utils/appError';
-import mongoose from 'mongoose';
+import { Request, Response, NextFunction } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import { Product } from "../models/productModel";
+import { AppError } from "../utils/appError";
+import mongoose from "mongoose";
+import { apiFeatures } from "../utils/apiFeatures";
 
 //- **GET /products** – Get all products
 
-const getAllProducts = catchAsync(async (_req, res, next) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    status: 'success',
+const getAllProducts = catchAsync(async (req, res, _next) => {
+  const features = new apiFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const products = await features.query;
+
+  res.status(200).json({
+    status: "success",
     length: products.length,
     data: {
       products,
@@ -22,16 +30,16 @@ const getOneProduct = catchAsync(async (req: Request, res, next) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return next(new AppError('Invalid product ID', 400));
+    return next(new AppError("Invalid product ID", 400));
   }
 
   const product = await Product.findById(id);
   if (!product) {
-    return next(new AppError('No product with this id', 404));
+    return next(new AppError("No product with this id", 404));
   }
 
   return res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
       product,
     },
